666c6fef7e052372f261d334e702a4ac34d229b6
ana <anaceci.perezmorales@gmail.com>
2018-09-12 03:47:52 -0500

    adding footer and header to blog, correct contact format

--- blog/wp-content/themes/bootstrap-blog/footer.php
+++ blog/wp-content/themes/bootstrap-blog/footer.php
@@ -8,16 +8,154 @@
  */
 
 ?>
-	<footer class="main">
+	<footer class="main footer2">
 		<div class="container">
 		<?php dynamic_sidebar( 'footer-1' ); ?>
 	</div>
 	</footer>
-		<div class="copyright text-center">
-			<?php esc_html_e( "Powered by", 'bootstrap-blog' ); ?> <a href="<?php echo esc_url( 'http://wordpress.org/' ); ?>"><?php esc_html_e( "WordPress", 'bootstrap-blog' ); ?></a> | <?php esc_html_e( 'Theme by', 'bootstrap-blog' ); ?> <a href="<?php echo esc_url( 'http://thebootstrapthemes.com/' ); ?>"><?php esc_html_e( 'TheBootstrapThemes','bootstrap-blog' ); ?></a>
-		</div>
-		<div class="scroll-top-wrapper"> <span class="scroll-top-inner"><i class="fa fa-2x fa-angle-up"></i></span></div> 
 		
 		<?php wp_footer(); ?>
 	</body>
+
+<style >
+footer {
+    /*position: fixed;*/
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 130px;
+    background-color: #633e84;
+    color: white;
+    text-align: center;
+    padding-top: 50px;
+}
+.footer2 {
+    /*position: fixed;*/
+   
+    background-color: #fff;
+   
+   
+}
+   /* nav styles */
+.nav-bar-page {
+  padding-right: 0px;
+  padding-left: 0px;
+  z-index: 2;
+}
+
+.nav-link:hover{
+ background: #f4f4f4;
+ color: #f97300;
+}
+.navbar{
+  padding-left: 0px;
+}
+
+
+.navbar-nav{
+color: #633e84;
+border-left: 0;
+font-weight: bold;
+font-size: 15px;
+z-index: 5;
+ padding-top: 1.1rem;
+}
+
+.nav-link , .navbar-brand{
+ color: #633e84;
+ cursor: pointer;
+ background-color: #633e84;
+
+ border-color: #633e84;
+ padding-top: 0rem;
+}
+
+
+.nav-link{
+ margin-right: 1em !important;
+}
+
+.nav-link:hover{
+ background: #f4f4f4;
+ color: #f97300;
+}
+.navbar-collapse{
+
+.navbar-toggler{
+  background:#fff !important;
+  right: 0;
+
+}
+
+.main-head-text {
+   margin: 70px auto;
+}
+}
+.footerr{
+  padding-top: 10px;
+}
+.footer-link{
+ color: white;
+ font-size: 12px;
+ font-weight: bold;
+ letter-spacing: 3px;
+ text-align: center;
+ cursor: pointer;
+ margin-right: 3em !important;
+
+}
+.navbar-inverse{
+	 background-color: #633e84;
+	 border-color: #633e84;
+}
+.white-text{
+	color:#fff;
+	 margin-right: 2em !important;
+}
+</style>
+   <footer class="page-footer font-small pt-4">
+      <div class="container-fluid  text-center">
+         <div class="row">
+            <div class="col-lg-12 navbar navbar-expand-lg navbar-default navbar-inverse border-0 rounded-0">
+               <!--  -->
+               <!-- <div class="menuContainer"> -->
+               <!-- <nav class="navbar navbar-expand-lg navbar-default navbar-inverse border-0 rounded-0">           -->
+               <div class="col-xl-3 col-md-2">
+                  <a class="navbar-brand-xl" href="index.html"><img src="/website/assets/images/wdatyra_logo.png" class="img-fluid  d-block" alt="responsive"></a>  
+               </div>
+               <div class=" d-lg-block">
+                  <ul class="navbar-nav align-items-center" >
+                     <li>
+                        <a class="footer-link" href="/website/services.html">SERVICES</a>
+                     </li >
+                     <li>
+                        <a class="footer-link" href="/website/portfolio.html">PORTFOLIO</a>
+                     </li>
+                     <li>
+                        <a class="footer-link" href="/website/blog/index.php">BLOG</a>
+                     </li>
+                     <li>
+                        <a class="footer-link" href="/website/about.html">ABOUT</a>
+                     </li>
+                     <li>
+                        <a class="footer-link" href="/website/contact.html">CONTACT</a>
+                     </li>
+                     <li>
+                        <a class="footer-link" href="/website/contact.html">CAREER</a>
+                     </li>
+                    
+                  </ul>
+               </div>
+               <div class="col-md-3">
+                  <!-- slack -->
+                  <a><i class="fa fa-slack white-text mr-md-3 mr-2 "></i></a>
+                  <!-- Twitter -->
+                  <a><i class="fa fa-twitter white-text mr-md-3 mr-2 "></i></a>
+                  <!-- Facebook -->
+                  <a><i class="fa fa-facebook white-text mr-md-3 mr-2 "></i></a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </footer>
 </html>
--- blog/wp-content/themes/bootstrap-blog/header.php
+++ blog/wp-content/themes/bootstrap-blog/header.php
@@ -14,31 +14,48 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="<?php echo esc_url( 'http://gmpg.org/xfn/11' ); ?>">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+      
+     
+  
 	<?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-
-<?php $menu_sticky = get_theme_mod( 'bootstrap_blog_header_sticky_menu_option', false ); ?>
-
-<?php
-	// Default values for 'bootstrap_blog_social_media' theme mod.
-	$defaults = "";
-	$social_media = get_theme_mod( 'bootstrap_blog_social_media', $defaults );
-?>
-
-<?php
-	set_query_var( 'menu_sticky', $menu_sticky );
-	set_query_var( 'social_media', $social_media );
-
-	$layout = get_theme_mod( 'bootstrap_blog_header_layouts', 'one' );
-    if( $layout == 'one' ) {
-		get_template_part( 'layouts/header/header-layout', 'one' );
-	}
 	
-?>
+	<div class="row  justify-content-between "style=" background-color: #fff;
+	 border-color: #fff;  padding-top: 5vw;margin-right: 0px;">
+               <div class="col-12 nav-bar-page" style="padding-left: 4vw;">
+                <div class="menuContainer">
+                  <nav class="offset-1 navbar navbar-expand-lg bg-light navbar-default navbar-inverse border-0 rounded-0" style=" background-color: #fff; border-color: #fff; flex;">
+                     <a class="navbar-brand col-3"  style=" background-color: #fff; border-color: #fff;"  href="/website/index.html"><img src="/website/assets/images/datyra_logo.png" class="img-responsive mx-auto d-block" alt="responsive"></a>
+                     
+                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbarNav" style="background:white;">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"style="background:gray;"></span>
+			        <span class="icon-bar"style="background:gray;"></span>
+			        <span class="icon-bar"style="background:gray;"></span>
+		      	</button>
+                     <div class="collapse navbar-collapse d-none d-block " id="navbarNav" >
+                           <ul class="navbar-nav " style="color: #fff; margin: auto; padding-left: 40vw;" >
+                                <li class="nav-item">
+                                  <a class="nav-link" style=" background-color: #fff; border-color: #fff; color: #633e84; padding-top: 0; padding-right: 0;margin-right: 3em !important;""href="/website/services.html">Services</a>
+                                </li>
+                                <li class="nav-item" style="">
+                                  <a class="nav-link " style=" background-color: #fff;	 border-color: #fff;color: #633e84;padding-top: 0; padding-right: 0;margin-right: 3em !important;"href="/website/portfolio.html">Portfolio</a>
+                                </li>
+                                <li class="nav-item" style="">
+                                  <a class="nav-link " style=" background-color: #fff;	 border-color: #fff; color: #633e84;padding-top: 0; padding-right: 0;margin-right: 3em !important;"href="/website/blog/index.php">Blog</a>
+                                </li>
+                                <li class="nav-item" style="">
+                                  <a class="nav-link " style=" background-color: #fff;	 border-color: #fff; color: #633e84;padding-top: 0; padding-right: 0;margin-right: 3em !important;"href="/website/about.html">About</a>
+                                </li>
+                                <li class="nav-item" style="">
+                                  <a class="nav-link " style=" background-color: #fff;	 border-color: #fff; color: #633e84;padding-top: 0; padding-right: 0;margin-right: 3em !important;"href="/website/contact.html">Contact</a>
+                                </li>
+                             </ul>
+                           </div>
+                        </nav>
+                      </div>
+                    </div>
+                </div>
+</head>
 
-<?php if ( class_exists( 'Breadcrumb_Trail' ) && ! is_home() && ! is_front_page() ) : ?>               
-	<div class="breadcrumbs">
-		<div class="container"><?php breadcrumb_trail(); ?></div>
-	</div>
-<?php endif; ?>
+<body <?php body_class(); ?>>
